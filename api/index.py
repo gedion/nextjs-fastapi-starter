6b8ffb12a3d9bfea5b2dd41e22dfa30f33b42c2f
Gedion Woldeselassie <gwy321@gmail.com>
2025-02-27 23:15:56 -0600

    feat: supervisor agent

--- api/index.py
+++ api/index.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+load_dotenv()  # take environment variables from .env.
 
+from fastapi import FastAPI
+from .agents.supervisor import supervisor
+
+
+print(os.getenv('OPENAI_API_KEY'))
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
-    return {"message": "Hello from FastAPI"}
+    result = supervisor.invoke({
+        "messages": [
+            {
+                "role": "user",
+                "content": "what's the combined headcount of the FAANG companies in 2024?"
+            }
+        ]
+    })
+    return result
